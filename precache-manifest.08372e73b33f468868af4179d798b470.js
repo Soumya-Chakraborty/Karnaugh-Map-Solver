2ee3d9a1c2f2e874b0a15e9fa156cd0254ef22f0
Soumya Chakraborty <soumyachakraborty198181@gmail.com>
2024-01-20 23:21:02 +0530

    Karnaugh-Map-Solver updated

---
+++ precache-manifest.08372e73b33f468868af4179d798b470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fc739b889617dc0080e3861ebd276b",
+    "url": "/Karnaugh-Map-Solver/index.html"
+  },
+  {
+    "revision": "e74949c3a29a319acb52",
+    "url": "/Karnaugh-Map-Solver/static/css/main.a5e43b1c.chunk.css"
+  },
+  {
+    "revision": "07d69f4be9c30b66f301",
+    "url": "/Karnaugh-Map-Solver/static/js/2.4e013504.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/Karnaugh-Map-Solver/static/js/2.4e013504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74949c3a29a319acb52",
+    "url": "/Karnaugh-Map-Solver/static/js/main.0daa67a1.chunk.js"
+  },
+  {
+    "revision": "49cdf123c43cd46279ef",
+    "url": "/Karnaugh-Map-Solver/static/js/runtime-main.a5aa32ab.js"
+  }
+]);
